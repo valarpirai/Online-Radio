af54a955cc06a2194435d2990d845f0c164766da
Valar <Valar@npcompete.net>
2017-04-13 17:09:22 +0530

    Fetching live radio list

--- js/radio.js
+++ js/radio.js
@@ -1,28 +1,37 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+$(document).ready(function() {
 
-    trigger.click(function () {
-      hamburger_cross();      
+    var liveRadioListUrl = "https://gist.githubusercontent.com/valarpirai/473305f09f8433f1d338634ed42c437d/raw/live-radio.json";
+
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+    trigger.click(function() {
+        hamburger_cross();
     });
 
     function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function() {
         $('#wrapper').toggleClass('toggled');
-  });  
+    });
+
+    $.ajax({
+        url : liveRadioListUrl
+    }).done(function(res) {
+        console.log(res);
+    });
 });
